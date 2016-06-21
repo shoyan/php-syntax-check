4a01157ed9a814ab417512f504a7d4837b207e70
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-06-21 15:38:54 +0900

    fix

--- check_syntax.sh
+++ check_syntax.sh
@@ -12,7 +12,7 @@ fi
 if [ -n "$CI_PULL_REQUESTS" ]; then
     git diff --name-only origin/master \
         | grep '.php' \
-        | xargs vendor/bin/phpcs  --standard=PSR2 --report=checkstyle \
+        | xargs vendor/bin/phpcs -n --standard=PSR2 --report=checkstyle \
         | bundle exec checkstyle_filter-git diff origin/master \
         | bundle exec saddler report \
         --require saddler/reporter/github \
