ddcf970a578c6cff5717f8ff174b6fd2cdc3b318
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-06-21 13:03:03 +0900

    Check syntax

--- check_syntax.sh
+++ check_syntax.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "Start"
-vendor/bin/phpcs index.php --standard=PSR2
+
+LIST=`git diff --name-only origin/master | grep '.php'`
+if [ -z $LIST ]; then
+    echo "Success"
+    exit 0
+fi
+
+vendor/bin/phpcs index.php --standard=PSR2 --report=checkstyle \
+ | checkstyle_filter-git diff origin/master
