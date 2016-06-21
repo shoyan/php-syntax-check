18df4b7f926dd50018320917a98592f1cd70ee8f
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-06-21 15:06:26 +0900

    fix

--- check_syntax.sh
+++ check_syntax.sh
@@ -10,8 +10,10 @@ fi
 echo $CI_PULL_REQUESTS
 echo $CI_PULL_REQUEST
 
-vendor/bin/phpcs index.php --standard=PSR2 --report=checkstyle \
- | bundle exec checkstyle_filter-git diff origin/master \
- | bundle exec saddler report \
-   --require saddler/reporter/github \
-   --reporter Saddler::Reporter::Github::PullRequestReviewComment
+if [ -n $CI_PULL_REQUESTS ]; then
+    vendor/bin/phpcs index.php --standard=PSR2 --report=checkstyle \
+        | bundle exec checkstyle_filter-git diff origin/master \
+        | bundle exec saddler report \
+        --require saddler/reporter/github \
+        --reporter Saddler::Reporter::Github::PullRequestReviewComment
+fi
