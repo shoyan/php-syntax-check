72152a1da3c98c4287227a86994b40b964bdcfce
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-06-21 13:56:55 +0900

    gem install

--- check_syntax.sh
+++ check_syntax.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 echo "Start"
-
 LIST=`git diff --name-only origin/master | grep '.php'`
 if [ -z $LIST ]; then
     echo "Success"
     exit 0
 fi
+
+gem install checkstyle_filter-git
 
 vendor/bin/phpcs index.php --standard=PSR2 --report=checkstyle \
  | checkstyle_filter-git diff origin/master
