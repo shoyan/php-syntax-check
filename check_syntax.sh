03c5e58ac0311125ac17d1e3bd0d5c112b76a7fa
Shohei Yamasaki <s-yamasaki@pepabo.com>
2016-06-21 15:17:59 +0900

    fix

--- check_syntax.sh
+++ check_syntax.sh
@@ -2,16 +2,19 @@
 
 echo "Start"
 LIST=`git diff --name-only origin/master | grep '.php'`
-if [ -z $LIST ]; then
-    echo "Success"
+
+if [ -z "$LIST" ]; then
+    echo "PHP file not changed."
     exit 0
 fi
 
 echo $CI_PULL_REQUESTS
 echo $CI_PULL_REQUEST
 
-vendor/bin/phpcs index.php --standard=PSR2 --report=checkstyle \
- | bundle exec checkstyle_filter-git diff origin/master \
- | bundle exec saddler report \
-   --require saddler/reporter/github \
-   --reporter Saddler::Reporter::Github::PullRequestReviewComment
+if [ -n "$CI_PULL_REQUESTS" ]; then
+    vendor/bin/phpcs index.php --standard=PSR2 --report=checkstyle \
+        | bundle exec checkstyle_filter-git diff origin/master \
+        | bundle exec saddler report \
+        --require saddler/reporter/github \
+        --reporter Saddler::Reporter::Github::PullRequestReviewComment
+fi
